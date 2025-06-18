2896fa395ae6de6b1dd23ccda7016e356fb55cff
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-18 15:12:33 +0530

    Update alb.tf

--- task7.terraform/alb.tf
+++ task7.terraform/alb.tf
@@ -1,3 +1,23 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP access to ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "strapi_alb" {
   name               = "strapi-alb"
   internal           = false
