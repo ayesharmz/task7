598acae8a2b1dce88d72b048c9393f1b83fcbdd2
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-18 15:07:00 +0530

    Create alb.tf

---
+++ task7.terraform/alb.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "strapi_alb" {
+  name               = "strapi-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_target_group" "strapi_tg" {
+  name     = "strapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-499"
+  }
+}
+
+resource "aws_lb_listener" "strapi_listener" {
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+  }
+}
